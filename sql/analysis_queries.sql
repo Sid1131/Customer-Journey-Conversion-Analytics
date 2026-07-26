4632ba5b6d1885b325a447554ec138f319f83d31
Siddharth Sinha <138559590+Sid1131@users.noreply.github.com>
2026-07-26 16:18:47 +0530

    Update analysis_queries.sql

--- sql/analysis_queries.sql
+++ sql/analysis_queries.sql
@@ -1,1 +1,19 @@
+/*==============================================================================
+Project : Customer Journey & Conversion Analytics
+Author  : Siddharth Sinha
+
+Description:
+This SQL script analyses customer browsing behaviour and purchasing
+patterns using the Online Shoppers Purchasing Intention dataset.
+
+Objectives:
+• Measure overall conversion performance
+• Understand visitor behaviour
+• Identify high-converting traffic sources
+• Analyse engagement metrics
+• Segment customers
+• Support business decision making
+
+==============================================================================*/
+
 
