d8d37508dd331c6a0594a5f92fa2d0af8270b95b
Siddharth Sinha <138559590+Sid1131@users.noreply.github.com>
2026-07-26 16:23:38 +0530

    Update analysis_queries.sql

--- sql/analysis_queries.sql
+++ sql/analysis_queries.sql
@@ -16,4 +16,12 @@ Objectives:
 
 ==============================================================================*/
 
+**Total Sessions** 
+select count(*) as total_sessions from online_shoppers
 
+** Conversion Rate**
+select 
+round(sum(case when Revenue='1' then 1 else 0 end)/count(*)*100,2) as conversion_rate 
+from online_shoppes
+
+**
